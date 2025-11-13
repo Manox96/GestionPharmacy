85c17e15686879f6251346d5e1591220748bb2ff
@Manox96 <aymanemanox15@gmail.com>
2025-11-13 14:00:48 +0100

    day one

---
+++ GestionPharmacy/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GestionPharmacy.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
